b698da6e70dec5f8a3ecdedb60fcc3576600ac99
Vanessa Jesik <vajesik@gmail.com>
2023-09-13 09:42:06 -0600

    fun with fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
+from cgi import print_arguments
+
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fibonacci = []
+    elif length == 1:
+        fibonacci = [0]
+    elif length == 2:
+        fibonacci = [0,1]
+    else:
+        fibonacci = [0,1]
+        i = 2
+        while i < length:
+            fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+            i += 1
+    print(fibonacci)
+        
